e3c66ace8ec4efe47eae844f79908611508f3ad1
Nafis Ahamed <nafisahamed14@gmail.com>
2024-03-20 16:01:33 +0600

    fourth commit

--- src/components/user/User.jsx
+++ src/components/user/User.jsx
@@ -1,12 +1,25 @@
+import { useLoaderData } from "react-router-dom";
+import Use from "../use/Use";
 
 const User = () => {
+
+    const users = useLoaderData();
+
     return (
         <div>
-            <h2>Our Users</h2>
+            <h2>Our Users: {users.length}</h2>
             <p>fantastic and vodro users</p>
+            
+            <div className="grid grid-cols-3 gap-2">
+                {
+                    users.map(user => <Use key={user.id} user={user}></Use> )
+                }
+            </div>
+
         </div>
     );
 };
+
 
 export default User;
 
